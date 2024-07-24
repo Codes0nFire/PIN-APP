eb78b90bc08d7f33a813eb62799676d0f1319cad
NoN Responsive WebDeveloper <gdbcodes@gmail.com>
2024-07-24 12:23:41 +0530

    same

--- routes/imagekit.js
+++ routes/imagekit.js
@@ -1,11 +1,12 @@
 // SDK initialization
 
 var ImageKit = require("imagekit");
+require("dotenv").config();
 
 var imagekit = new ImageKit({
-    publicKey : "public_WnupJeBkMx0i0tOg3BzMc5imUgQ=",
-    privateKey : "private_5H9lOerVMVM39KR30nrzwxtqHT8=",
-    urlEndpoint : "https://ik.imagekit.io/CodesOnFIre69/"
+    publicKey : `${process.env.PUBLICKEY}`,
+    privateKey : `${process.env.PRIVATEKEY}`,
+    urlEndpoint : `${process.env.URLENDPOINT}`
 });
 
 
